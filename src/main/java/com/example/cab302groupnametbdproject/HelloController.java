c2cda9c4fb0d7894344663244b1a691a764790ce
Lucas Gorrie <n11277211@qut.edu.au>
2024-03-23 11:22:29 +1000

    first commit

---
+++ src/main/java/com/example/cab302groupnametbdproject/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.cab302groupnametbdproject;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.cab302groupnametbdproject {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.cab302groupnametbdproject to javafx.fxml;
+    exports com.example.cab302groupnametbdproject;
+}
