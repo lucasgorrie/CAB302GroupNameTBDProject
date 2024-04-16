4190755ab632ac921e9fe4865a2ebdd1a70d2bce
Logan Kocon <logankocon@gmail.com>
2024-04-16 12:40:12 +1000

    added some ui including home page and functionality, very bare bones, mostly just getting used to javaFX

--- src/main/java/com/example/cab302groupnametbdproject/HelloController.java
+++ src/main/java/com/example/cab302groupnametbdproject/HelloController.java
@@ -1,14 +1,35 @@
 package com.example.cab302groupnametbdproject;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
+    @FXML
+    private Button continueButton;
+    @FXML
+    private Button loginBackButton;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void onContinueButtonClick() throws IOException {
+            Stage stage = (Stage) continueButton.getScene().getWindow();
+            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("login-view.fxml"));
+            Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+            stage.setScene(scene);
+    }
+
+    public void onLoginBackButtonClick() throws IOException {
+        Stage stage = (Stage) loginBackButton.getScene().getWindow();
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setScene(scene);
     }
 }
