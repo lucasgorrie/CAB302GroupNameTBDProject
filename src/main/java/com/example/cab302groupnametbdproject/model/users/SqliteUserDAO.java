39a841b0ae392dae068f0195d4e0fe413056bdcd
Lucas Gorrie <n11277211@qut.edu.au>
2024-04-23 16:27:07 +1000

    Finalised basic User DAO, CRUD operations implemented

--- src/main/java/com/example/cab302groupnametbdproject/model/users/SqliteUserDAO.java
+++ src/main/java/com/example/cab302groupnametbdproject/model/users/SqliteUserDAO.java
@@ -3,7 +3,10 @@ package com.example.cab302groupnametbdproject.model.users;
 import com.example.cab302groupnametbdproject.model.SqliteConnection;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 // Handles DAO and CRUD
@@ -17,10 +20,11 @@ public class SqliteUserDAO implements UserDAO {
 
         // Insert data for testing
         insertTestingData();
+
     }
 
+    // Create the users table, if it doesn't already exist
     private void createTable() {
-        // Create the users table, if it doesn't already exist
         try {
             Statement statement = connection.createStatement();
             String query = "CREATE TABLE IF NOT EXISTS users ("
@@ -56,33 +60,94 @@ public class SqliteUserDAO implements UserDAO {
         }
     }
 
-    // Needs finishing, not started
+    // Adds a new User object to the users table
     @Override
     public void addUser(User user) {
-
+        try {
+            PreparedStatement statement = connection.prepareStatement("INSERT INTO users (firstName, lastName, email, password) VALUES (?, ?, ?, ?)");
+            statement.setString(1, user.getFirstName());
+            statement.setString(2, user.getLastName());
+            statement.setString(3, user.getEmail());
+            statement.setString(4, user.getPassword());
+            statement.executeUpdate();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
-    // Needs finishing, not started
+    // Updates a User's data in the users table
     @Override
     public void updateUser(User user) {
-
+        try {
+            PreparedStatement statement = connection.prepareStatement("UPDATE users SET firstName = ?, lastName = ?, email = ?, password = ? WHERE id = ?");
+            statement.setString(1, user.getFirstName());
+            statement.setString(2, user.getLastName());
+            statement.setString(3, user.getEmail());
+            statement.setString(4, user.getPassword());
+            statement.setInt(5, user.getId());
+            statement.executeUpdate();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     // Needs finishing, not started
     @Override
     public void deleteUser(User user) {
-
+        try {
+            PreparedStatement statement = connection.prepareStatement("DELETE FROM users WHERE id = ?");
+            statement.setInt(1, user.getId());
+            statement.executeUpdate();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
-    // Needs finishing, not started
+    // Returns a single User object based on an ID argument
     @Override
     public User getUser(int id) {
+        try {
+            PreparedStatement statement = connection.prepareStatement("SELECT * FROM users WHERE id = ?");
+            statement.setInt(1, id);
+            ResultSet resultSet = statement.executeQuery();
+            if (resultSet.next()) {
+                String firstName = resultSet.getString("firstName");
+                String lastName = resultSet.getString("lastName");
+                String email = resultSet.getString("email");
+                String password = resultSet.getString("password");
+                User user = new User(firstName, lastName, email, password);
+                user.setId(id);
+                return user;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         return null;
     }
 
-    // Needs finishing, not started
+    // Returns a list containing User objects of all the users in the DB table "users"
+    // On its own, this will only return the objects themselves. Use something like this to get specific properties:
+    // List<String> firstNames = getAllUsers().stream().map(User::getFirstName).toList(); <-- gets all first names
     @Override
     public List<User> getAllUsers() {
-        return null;
+        List<User> users = new ArrayList<>();
+        try {
+            Statement statement = connection.createStatement();
+            String query = "SELECT * FROM users";
+            ResultSet resultSet = statement.executeQuery(query);
+            while (resultSet.next()) {
+                int id = resultSet.getInt("id");
+                String firstName = resultSet.getString("firstName");
+                String lastName = resultSet.getString("lastName");
+                String email = resultSet.getString("email");
+                String password = resultSet.getString("password");
+                User user = new User(firstName, lastName, email, password);
+                user.setId(id);
+                users.add(user);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return users;
     }
 }
--- src/main/java/com/example/cab302groupnametbdproject/model/users/User.java
+++ src/main/java/com/example/cab302groupnametbdproject/model/users/User.java
@@ -16,45 +16,114 @@ public class User {
         this.password = password;
     }
 
+    /**
+     *
+     * @return the id of the User
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     *
+     * @param id the id to be set for the User
+     */
     public void setId(int id) {
         this.id = id;
     }
 
+    /**
+     *
+     * @return the first name of the User
+     */
     public String getFirstName() {
         return firstName;
     }
 
+    /**
+     *
+     * @param firstName the name to be set for the User
+     */
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
 
+    /**
+     *
+     * @return the last name of the User
+     */
     public String getLastName() {
         return lastName;
     }
 
+    /**
+     *
+     * @param lastName the last name to be set for the User
+     */
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
 
+    /**
+     *
+     * @return the email of the User
+     */
     public String getEmail() {
         return email;
     }
 
+    /**
+     *
+     * @param email the email to be set for the User
+     */
     public void setEmail(String email) {
         this.email = email;
     }
 
+    /**
+     *
+     * @return the first name and the last name of the User
+     */
     public String getFullName() {
         return firstName + " " + lastName;
     }
 
+    /**
+     *
+     * @return the application password of the User
+     */
+    public String getPassword(){
+        return password;
+    }
+
+    /**
+     *
+     * @param password the password to be set for the User
+     */
     public void setPassword(String password){this.password = password;}
 
+    /**
+     * Encrypts the application password of the User
+     */
+    public void encryptUserPassword(){
+        // ADD FUNCTIONALITY TO ENCRYPT USER APPLICATION PASSWORD
+    }
+
+    /**
+     * Decrypts the application password of the User
+     */
+    public void decryptUserPassword(){
+        // ADD FUNCTIONALITY TO DECRYPT USER APPLICATION PASSWORD
+    }
+
+    /**
+     *
+     * @param passwordEntered the password entered by the user into the login form
+     * @return whether the input matches the User's application password
+     * May need to be updated to call the @decryptUserPassword function later down the line
+     */
     public boolean UserPasswordCorrect(String passwordEntered){
         return this.password == passwordEntered;
     }
+
 }
