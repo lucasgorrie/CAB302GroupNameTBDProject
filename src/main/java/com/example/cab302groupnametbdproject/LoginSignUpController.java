23fd762e989b0695fb1bcb54081e5decb73d3752
Logan Kocon <logankocon@gmail.com>
2024-04-26 23:44:11 +1000

    signup ui now implemented!

--- src/main/java/com/example/cab302groupnametbdproject/LoginSignUpController.java
+++ src/main/java/com/example/cab302groupnametbdproject/LoginSignUpController.java
@@ -13,12 +13,29 @@ public class LoginSignUpController {
 
     @FXML
     private Button createUserButton;
+    @FXML
+    private Button backToLoginButton;
+    @FXML
+    private Label loginInfo;
+    @FXML
+    private Label signupInfo;
 
+
+    //button to take user to signup view when hitting create user button
     @FXML
     protected void onCreateUserButtonClick() throws IOException {
         Stage stage = (Stage) createUserButton.getScene().getWindow();
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("signup-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setScene(scene);
+    }
+
+    //button to take the user back to the login page from the signup page
+    @FXML
+    protected void onBackToLoginClick() throws IOException {
+        Stage stage = (Stage) backToLoginButton.getScene().getWindow();
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("login-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load());
         stage.setScene(scene);
     }
 
