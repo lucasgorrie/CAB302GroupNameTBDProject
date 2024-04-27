051a6f108d9dfa774a55741637d10279ff0179ce
Jmillsyy <126749475+Jmillsyy@users.noreply.github.com>
2024-04-27 21:35:21 +1000

    touched up UI colours

--- src/main/java/com/example/cab302groupnametbdproject/HelloController.java
+++ src/main/java/com/example/cab302groupnametbdproject/HelloController.java
@@ -12,39 +12,41 @@ import javafx.scene.control.Button;
 
 public class HelloController {
 
-    public Label titleLabel;
     @FXML
     private BorderPane mainLayout;
     @FXML
     private VBox mainContent;
 
-    private void switchToPage(String labelText) {
-        Label pageLabel = new Label(labelText);
-        pageLabel.setAlignment(Pos.CENTER);
+    private void switchToPage(String pageName) {
+        mainLayout.setCenter(createContentPage(pageName));
+    }
 
-        VBox centerContent = new VBox(pageLabel);
-        centerContent.setAlignment(Pos.CENTER);
+    private BorderPane createContentPage(String pageName) {
+        Label titleLabel = new Label(pageName);
+        titleLabel.setAlignment(Pos.CENTER);
 
-        // Use the custom BackButton class
+        VBox content = new VBox(titleLabel);
+        content.setAlignment(Pos.CENTER);
+
         BackButton backButton = new BackButton(mainLayout, mainContent);
 
-        BorderPane newPage = new BorderPane();
-        newPage.setTop(backButton);
-        newPage.setCenter(centerContent);
+        BorderPane pageLayout = new BorderPane();
+        pageLayout.setTop(backButton);
+        pageLayout.setCenter(content); // You can customize content here based on pageName
 
-        mainLayout.setCenter(newPage);
+        return pageLayout;
     }
 
     public static class BackButton extends Button {
-
         public BackButton(BorderPane mainLayout, Pane mainContent) {
             super("Back");
             this.setOnAction(event -> mainLayout.setCenter(mainContent));
-            this.setStyle("-fx-background-color: #3071a9; -fx-text-fill: white;");
+            this.setStyle("-fx-background-color: #FFC04C; -fx-text-fill: black;");
             BorderPane.setMargin(this, new Insets(10));
             BorderPane.setAlignment(this, Pos.TOP_LEFT);
         }
     }
+
     @FXML
     private void onSignOutButtonClick() {
         switchToPage("Sign Out");
