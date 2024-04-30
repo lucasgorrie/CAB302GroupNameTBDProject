58810316c5f56ce9425b855c3c81d5c7f68c2e9e
Lucas Gorrie <n11277211@qut.edu.au>
2024-04-30 11:45:08 +1000

    Added Website object method testing

---
+++ src/main/java/com/example/cab302groupnametbdproject/model/associatedWebsites/WebsiteTest.java
@@ -0,0 +1,26 @@
+package com.example.cab302groupnametbdproject.model.associatedWebsites;
+
+import com.example.cab302groupnametbdproject.model.associatedWebsites.Website;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class WebsiteTest {
+    private Website website;
+    @BeforeEach
+    public void setUp() {
+        website = new Website("Bing.com");
+    }
+    @Test
+    public void testGetSetId() {
+        website.setId(9);
+        assertEquals(9, website.getId());
+    }
+
+    @Test
+    public void testGetSetURL(){
+        website.setURL("google.com");
+        assertEquals("google.com", website.getURL());
+    }
+
+}
