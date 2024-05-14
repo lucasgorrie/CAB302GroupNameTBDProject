f5ae8c81a84deff63a38be56c83c8067a18d0f74
Lucas Gorrie <n11277211@qut.edu.au>
2024-05-14 16:03:27 +1000

    test

--- src/main/java/com/example/cab302groupnametbdproject/HelloApplication.java
+++ src/main/java/com/example/cab302groupnametbdproject/HelloApplication.java
@@ -20,6 +20,7 @@ public class HelloApplication extends Application {
         stage.show();
 
         scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("style2.css")).toExternalForm());
+        int j;
     }
 
     public static void main(String[] args) {
