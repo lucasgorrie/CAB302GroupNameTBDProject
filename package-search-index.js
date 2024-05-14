8920918f08e13056d2b6c70eca7a36ab461f9e8d
lucasgorrie <115451070+lucasgorrie@users.noreply.github.com>
2024-05-14 12:57:50 +0000

    Deploying to gh-pages from @ lucasgorrie/CAB302GroupNameTBDProject@a5d20b399e639a57ccf2029e803f2c413d42f33b 🚀

---
+++ package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"com.example.cab302groupnametbdproject","l":"com.example.cab302groupnametbdproject"},{"m":"com.example.cab302groupnametbdproject","l":"com.example.cab302groupnametbdproject.controllers"}];updateSearchResults();
